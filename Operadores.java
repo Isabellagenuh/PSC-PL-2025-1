35a471b4dea1d82f7206d646afc1e48c0b5b6862
isabellagenuh <isabellageenuino@gmail.com>
2025-03-07 11:34:27 -0300

    Criação dos Operadores na Classe

--- Operadores.java
+++ Operadores.java
@@ -1,5 +1,46 @@
 public class Operadores{
     public static void main(String[] args) {
+       // Operadores aritimeticos|
+
+
+       int a = 10;
+       int b = 5;
+       System.out.println(a + b);
+       System.out.println(a - b);
+       System.out.println(a / b);
+       System.out.println(a * b);
+       System.out.println(a % b);
+
+
+       // Operadores de comparação |
+
+       int c = 54;
+       int d = 65;
+       System.out.println(c == d);
+       System.out.println(c != d);
+       System.out.println(c >= d);
+       System.out.println(c <= d);
+       System.out.println(c > d);
+       System.out.println(c < d);
+
+       // Operadores Lógicos |
+
+       boolean e = true;
+       boolean f = false;
+       System.out.println(e && f);
+       System.out.println(e || f);
+       System.out.println(!e);
+
+       // Operadores de Atribuição |
+
+       int g = 10;
+       g += 5; // g = g + 5 = 15
+       g -= 5; // g = g - 5 = 5
+       g /= 5; // g = g / 5 = 2
+       g *= 5; // g = g * 5 = 50
+       g %= 5; // g = g % 5 = 15
+
+
         System.out.println("Olá Mundo");
     }
 }
